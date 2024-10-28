fb3d62232c39d0c70ab233925e9de414459d51b3
虞航 <714610354@qq.com>
2024-10-28 13:29:57 +0800

    更新代码

--- docker/ubuntu20/run.sh
+++ docker/ubuntu20/run.sh
@@ -1,4 +1,4 @@
 docker stop zlm
 docker rm zlm
-docker run -d --name zlm -v /etc/yum.repos.d/openEuler.repo:/root/docker/openEuler.repo  -v /root/docker/ZLMediaKit:/root/ZLMediaKit  zlm:latest
+docker run -d --name zlm -v /root/docker/openEuler.repo:/etc/yum.repos.d/openEuler.repo  -v /root/docker/ZLMediaKit:/root/ZLMediaKit  zlm:latest
 docker exec -it zlm /bin/bash
